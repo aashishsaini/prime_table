efaed851b81b981c7a4de809bd12a41b57817542
Aashish Saini <aashish.saini@3pillarglobal.com>
2018-05-20 11:48:57 +0530

    modify generator

--- lib/generators/prime_table/install_generator.rb
+++ lib/generators/prime_table/install_generator.rb
@@ -1,7 +1,7 @@
 module PrimeTable
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../table_generator_example.rb", __FILE__)
+      source_root File.expand_path("../", __FILE__)
       desc "copies the example rb to root directory"
 
       def copy_initializer
