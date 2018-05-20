10c424c8c6fac13a49d449f2af78a14495aded9b
Aashish Saini <aashish.saini@3pillarglobal.com>
2018-05-20 10:44:46 +0530

    add generator

---
+++ lib/generators/prime_table/install_generator.rb
@@ -0,0 +1,12 @@
+module PrimeTable
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../", __FILE__)
+      desc "copies the example rb to root directory"
+
+      def copy_initializer
+        template 'table_generator_example.rb', '../'
+      end
+    end
+  end
+end
